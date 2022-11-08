96ca1dab2e289a0f287b3ef2f5e626f4e48390fa
Marcus Smith <marcushsmith@comcast.net>
2022-11-08 23:43:15 +0100

    add google analytics

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,12 +1,31 @@
+import Script from 'next/script'
+
 import Layout from '../components/layout';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"/>
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-9911244-2', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
