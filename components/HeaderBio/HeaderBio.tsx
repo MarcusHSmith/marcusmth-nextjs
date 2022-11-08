52072f20188d90cb168ea54cd21df7d6b07052a5
Marcus Smith <marcushsmith@comcast.net>
2022-11-08 18:10:39 +0100

    fix import

--- components/HeaderBio/HeaderBio.tsx
+++ components/HeaderBio/HeaderBio.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import Image from "next/Image";
+import Image from 'next/image';
 import { ReactElement, useMemo } from "react";
 import { FULL_NAME } from "../../utils/constants";
 
