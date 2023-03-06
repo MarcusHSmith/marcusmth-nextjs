4a257cb636274a12237293e15acd5b9d2ea8c61d
Marcus Smith <marcushsmith@comcast.net>
2023-03-05 20:54:19 -0500

    update google analytics

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,27 +1,26 @@
-import Script from 'next/script'
+import Script from "next/script";
 
-import Layout from '../components/layout';
+import Layout from "../components/layout";
 
-import '../styles/globals.css';
+import "../styles/globals.css";
+
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"/>
       <Script
-        id='google-analytics'
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'UA-9911244-2', {
-              page_path: window.location.pathname,
-            });
-          `,
-          }}
       />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){window.dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${GA_MEASUREMENT_ID}');
+  `}
+      </Script>
       <Layout>
         <Component {...pageProps} />
       </Layout>
