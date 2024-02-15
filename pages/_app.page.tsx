04898d7494c54fc797f5c32ad82796fdbe8eb9af
Marcus Smith <marcushsmith@comcast.net>
2024-02-15 14:24:28 -0800

    google analytics

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,6 +1,5 @@
-import Script from "next/script";
-
 import Layout from "../components/layout";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 import "../styles/globals.css";
 
@@ -9,21 +8,10 @@ const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){window.dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', '${GA_MEASUREMENT_ID}');
-  `}
-      </Script>
       <Layout>
         <Component {...pageProps} />
       </Layout>
+      <GoogleAnalytics gaId={GA_MEASUREMENT_ID} />
     </>
   );
 }
