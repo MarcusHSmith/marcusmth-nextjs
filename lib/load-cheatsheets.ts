15d8c8678bc7601ecb827637c0ad9b2ab1cd3aed
Marcus Smith <marcushsmith@comcast.net>
2024-02-16 17:48:47 -0800

    cheatsheet

--- lib/load-cheatsheets.ts
+++ lib/load-cheatsheets.ts
@@ -1,7 +1,9 @@
 import fs from "fs";
+import path from "path";
 
 export async function loadCheatsheets() {
-  const files = fs.readdirSync("content/cheatsheet");
+  const dir = path.resolve(process.cwd(), "content/cheatsheet");
+  const files = fs.readdirSync(dir);
   const cheatsheets = files.map((fileName) => {
     return fileName.replace(".md", "");
   });
--- lib/load-posts.ts
+++ lib/load-posts.ts
@@ -3,7 +3,6 @@ import path from "path";
 
 export async function loadPosts() {
   const dir = path.resolve(process.cwd(), "content/blog");
-
   const files = fs.readdirSync(dir);
   const posts = files.map((fileName) => {
     return fileName.replace(".md", "");
