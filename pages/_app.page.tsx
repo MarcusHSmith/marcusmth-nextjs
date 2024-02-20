f5339f171ca89ee3121d7bf71f4f80c1ea2efa47
Marcus Smith <marcushsmith@comcast.net>
2024-02-19 22:17:39 -0800

    add open graph meta headers

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,13 +1,34 @@
 import Layout from "../components/layout";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Head from "next/head";
 
 import "../styles/globals.css";
 
 const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
 
 function MyApp({ Component, pageProps }) {
+  const { frontmatter } = pageProps;
+  const { title, description, featuredImage, tags } = frontmatter || {};
+
+  const titleWithFallback = title || "marcusmth";
+  const descriptionWithFallback =
+    description || "Marcus's thoughts and tools as a software engineer.";
+  const ogImageWithFallback = featuredImage?.src
+    ? `https://marcusmth.com/images/${featuredImage.src}`
+    : "https://marcusmth.com/images/profile-pic-marcus.jpg";
+  const ogKeywordsWithFallback =
+    tags || "marcus smith software engineer blog youtube analytics";
+
   return (
     <>
+      <Head>
+        <title>{titleWithFallback}</title>
+        <meta property="og:title" content={titleWithFallback} />
+        <meta property="og:image" content={ogImageWithFallback} />
+        <meta property="description" content={descriptionWithFallback} />
+        <meta property="og:description" content={descriptionWithFallback} />
+        <meta name="keywords" content={ogKeywordsWithFallback}></meta>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
