d1e2f0af4ec2b955f1e1e2300fe915e9ea1d9df8
Marcus Smith <marcushsmith@comcast.net>
2025-05-28 15:58:23 -0300

    fix about

--- components/HeaderBio/HeaderBio.tsx
+++ components/HeaderBio/HeaderBio.tsx
@@ -37,7 +37,6 @@ export function HeaderBio({
               for information learned and utilized.
             </span>
             <InternalLinks />
-            <ExternalLinks />
           </div>
         );
     }
@@ -73,36 +72,11 @@ function InternalLinks(): ReactElement {
           <span>Cheatsheets</span>
         </Link>
       )}
+      {router.pathname !== "/about" && (
+        <Link href={`/about`}>
+          <span>About Marcus</span>
+        </Link>
+      )}
     </div>
-  );
-}
-
-function ExternalLinks(): ReactElement {
-  return (
-    <div className="flex flex-wrap gap-2">
-      <CustomLink
-        url={`https://www.youtube.com/channel/UCzLPnJlM_5IEe2djVMB2jLA`}
-        text={`Youtube`}
-      />
-      <CustomLink url={`https://twitter.com/marcusmth`} text={`Twitter`} />
-      <CustomLink
-        url={`https://www.strava.com/athletes/9512`}
-        text={`Strava`}
-      />
-    </div>
-  );
-}
-
-function CustomLink({
-  url,
-  text,
-}: {
-  url: string;
-  text: string;
-}): ReactElement {
-  return (
-    <Link className="h-8 min-w-12 flex-item" href={url}>
-      <p>{text}</p>
-    </Link>
   );
 }
