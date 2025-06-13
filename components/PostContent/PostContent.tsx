94c0ed52eb90d8f1847a1c885cb30a07aec2464a
Marcus Smith <marcushsmith@comcast.net>
2025-06-13 08:21:55 -0700

    padding on posts

--- components/PostContent/PostContent.tsx
+++ components/PostContent/PostContent.tsx
@@ -64,7 +64,7 @@ export default function PostPage({ frontmatter, content }) {
   };
 
   return (
-    <div className="max-w-4xl mx-auto">
+    <div className="max-w-4xl mx-auto px-4 md:px-8">
       <Head>
         <script
           type="application/ld+json"
